c035e2827398f64344fa1af9c06f88c47d4e7b4e
Moataz Ahmed <moataz.ahmed@leanix.net>
2021-07-23 13:09:03 -0400

    send data to MI workspace again

--- build/build_script.sh
+++ build/build_script.sh
@@ -1,5 +1,5 @@
 # If MI_DEV_HOST isn't set in travis, a default value is used
-export HOST=${MI_DEV_HOST:demo-us.leanix.net}
+export HOST=${MI_DEV_HOST:-demo-us.leanix.net}
 
 # Check if token env variable is set in travis
 if [[ -z "${MI_DEV_TOKEN}" ]]; then
