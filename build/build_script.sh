8f63c1b58bec5fb2b60475be40ce1e4db4f6342b
Moataz Ahmed <moataz.ahmed@leanix.net>
2022-03-04 11:54:08 -0500

    try #2.5

--- build/build_script.sh
+++ build/build_script.sh
@@ -3,13 +3,13 @@ export HOST=${VSM_DEV_HOST:-demo-us.leanix.net}
 
 # Check if token env variable is set in travis
 if [[ -z "${VSM_DEV_TOKEN}" ]]; then
-    echo "Please add your MI workspace token as an env variable 'MI_DEV_TOKEN' in travis."
+    echo "Please add your workspace token as an env variable 'MI_DEV_TOKEN' in travis."
     exit 1
 fi
 
 # Fetch bearer token
 export SYNC_URL="https://${HOST}/services/integration-api/v1/synchronizationRuns"
-TOKEN=$(curl -X POST --url https://${HOST}/services/mtm/v1/oauth2/token -u apitoken:${MI_DEV_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
+TOKEN=$(curl -X POST --url https://${HOST}/services/mtm/v1/oauth2/token -u apitoken:${VSM_DEV_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
 
 # Run license-checker
 license-checker --json > $TRAVIS_BUILD_DIR/build/dependencies.json
@@ -27,8 +27,8 @@ curl -X POST \
   -F dependencies=@"$TRAVIS_BUILD_DIR/build/dependencies.json" \
   -F manifest=@"$TRAVIS_BUILD_DIR/lx-manifest.yaml" \
   -F 'data={
-  "version": "1.2.0",
-  "stage": "stg",
+  "version": "1.1.0",
+  "stage": "dev",
   "dependencyManager": "NPM"
 }' \
   https://$HOST/services/cicd-connector/v2/deployment
